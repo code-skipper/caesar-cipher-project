ce5be9821edab70f2938f835427326f0d468e95f
Thiago Mocarzel <108438695+code-skipper@users.noreply.github.com>
2024-02-28 17:11:01 -0300

    Update main.py
    Added a logo and improved user experience.

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
+import art
+print(art.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
+# Define your ceaser function without the loop
 def ceaser(plain_text, shift_amount, direction):
-  message = ''
-  for letter in plain_text:
-      if letter in alphabet:
-          current_index = alphabet.index(letter)
-          # Apply the shift
-          if direction == 'encode':
-              new_index = (current_index + shift_amount) % 26
-          elif direction == 'decode':
-              new_index = (current_index - shift_amount) % 26
-          shifted_letter = alphabet[new_index]
-          message += shifted_letter
-      else:
-          # Directly add the character if it's not in the alphabet
-          message += letter
-  print(f'Your {direction}d message: {message}')
+    message = ''
+    for letter in plain_text:
+        if letter in alphabet:
+            current_index = alphabet.index(letter)
+            if direction == 'encode':
+                new_index = (current_index + shift_amount) % 26
+            elif direction == 'decode':
+                new_index = (current_index - shift_amount) % 26
+            shifted_letter = alphabet[new_index]
+            message += shifted_letter
+        else:
+            message += letter
+    print(f'Your {direction}d message: {message}')
 
-# Now calling the ceaser function with direction as an argument as well
-ceaser(text, shift, direction)
+# Use a while loop here to keep asking the user if they want to continue
+run = True
+while run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    ceaser(text, shift, direction)
+
+    replay = input("Type 'yes' if you want to go again. Otherwise type 'no': ").lower()
+    if replay != 'yes':
+        run = False
+        print('Goodbye!')
